8255861dde7d5cb458769294bf9f99982d74fc23
Bryan Le <bryan.le7826@gmail.com>
2021-04-29 19:24:21 -0500

    Added script of making Pokemon and finishing scripting for Grass

---
+++ Assets/Scripts/GrassPatchHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrassPatchHandler : MonoBehaviour
+{
+
+    void OnTriggerEnter(Collider other)
+    {
+        CheckforEncounters();
+    }
+
+    public void CheckforEncounters()
+    {
+        if (Random.Range(1, 101) <= 10)
+        {
+            Debug.Log("Encounterned Wild Pokemon");
+        }
+    }
+}
